bfd027f1987a2d73c58f205a70dde573b869a2e0
Zack Jorquera <jorquerazack@gmail.com>
2017-03-14 16:47:45 -0600

    Update SpectralDataPoint.cs

--- SpectralDataPoint.cs
+++ SpectralDataPoint.cs
@@ -18,7 +18,8 @@ namespace MarsImageThing
     class SpectralData
     {
         public Microsoft.Xna.Framework.Point point = new Microsoft.Xna.Framework.Point();
-        Stream spectralDataPlotLocation;
+        public Stream spectralDataPlotLocation;
+        public String fileName;
         int cameraImagesAreFrom;
         int[] SpectralWaveLengthForEachFilterLeft = { 739, 753, 673, 601, 535, 482, 432, 440 }; //does not include L0
         int[] SpectralWaveLengthForEachFilterRight = { 436, 754, 803, 864, 904, 934, 1009, 880 };//or R0
@@ -27,6 +28,10 @@ namespace MarsImageThing
         {
             point = Point;
             spectralDataPlotLocation = SpectralDataPlotLocation;
+
+            if (spectralDataPlotLocation != null)
+                fileName = (spectralDataPlotLocation as FileStream).Name.Split('\\').Last().Split('.').First();//((FileStream)spectralDataPlotLocation) would of also work
+
             cameraImagesAreFrom = CameraImagesAreFrom;
         }
         public float[] GetReflectanceVectorFromPlotFile()
@@ -42,67 +47,82 @@ namespace MarsImageThing
                 WaveLengths.Concat(SpectralWaveLengthForEachFilterRight.ToList());
             }
 
-            float[] vector = new float[WaveLengths.Count];
+            float[,] vector = new float[WaveLengths.Count, 2];
+            for (int i = 0; i < vector.Length / 2; i ++ )
+            {
+                vector[i, 1] = 100;
+            }
 
-                using (StreamReader SR = new StreamReader(spectralDataPlotLocation))
+            using (StreamReader SR = new StreamReader(spectralDataPlotLocation))
+            {
+                char[] fullText = SR.ReadToEnd().ToCharArray();
+                int dataPointNum = 0;
+                bool gettingPoint = false;
+                bool startLookingForData = false;
+                string[] currentDataPoint = { "", "", "" };
+                for (int i = 0; i < fullText.LongLength; i++)
                 {
-                    char[] fullText = SR.ReadToEnd().ToCharArray();
-                    int dataPointNum = 0;
-                    bool gettingPoint = false;
-                    bool startLookingForData = false;
-                    string[] currentDataPoint = { "", "", "" };
-                    for (int i = 0; i < fullText.LongLength; i++)
+                    if (!startLookingForData)
                     {
-                        if (!startLookingForData)
+                        if (i + 15 >= fullText.LongLength)
+                            break;
+                        startLookingForData = true;
+                        for (int j = 0; j < 14; j++)//looks for 14 [Spaces] or [New Line] before starting to collect data. this avoides the junk that the begining
                         {
-                            if (i + 15 >= fullText.LongLength)
-                                break;
-                            startLookingForData = true;
-                            for (int j = 0; j < 14; j++)//looks for 14 [Spaces] or [New Line] before starting to collect data. this avoides the junk that the begining
-                            {
-                                if (fullText[i + j] != (char)32 && fullText[i + j] != (char)10)
-                                    startLookingForData = false;
-                            }
-                            continue;
+                            if (fullText[i + j] != (char)32 && fullText[i + j] != (char)10)
+                                startLookingForData = false;
                         }
+                        continue;
+                    }
 
-                        if (fullText[i] == (char)32 || fullText[i] == (char)10 || i == fullText.LongLength - 1)
+                    if (fullText[i] == (char)32 || fullText[i] == (char)10 || i == fullText.LongLength - 1)
+                    {
+                        if (gettingPoint)
                         {
-                            if (gettingPoint)
+                            if (!(currentDataPoint[dataPointNum % 3].Length <= 2 || currentDataPoint[0] == "-1.23e34"))
                             {
-                                if (currentDataPoint[dataPointNum % 3].Length <= 2 || currentDataPoint[0] == "-1.23e34")
-                                    continue;
-                                if (dataPointNum % 3 == 2)
+                                if (dataPointNum % 3 == 2)//why 2
                                 {
-
-                                    float waveLength = float.Parse(currentDataPoint[0]) * 1000;
+                                    float waveLengthFromFile = float.Parse(currentDataPoint[0]) * 1000;
                                     for (int j = 0; j < WaveLengths.Count; j++)
                                     {
-                                        float waveLengthToGet = (float)WaveLengths[j];
+                                        if (currentDataPoint[1] == "-1.23e34")
+                                            continue;
 
-                                        if (waveLength == waveLengthToGet)
+                                        float waveLengthToBe = WaveLengths[j];
+
+                                        if (Math.Abs(waveLengthFromFile - waveLengthToBe) < vector[j, 1])
                                         {
-                                            vector[j] = float.Parse(currentDataPoint[1]);
+                                            vector[j, 0] = float.Parse(currentDataPoint[1]);
+                                            vector[j, 1] = Math.Abs(waveLengthFromFile - waveLengthToBe);
                                         }
                                     }
                                 }
-                                dataPointNum++;
+                            }
+                            dataPointNum++;
 
-                            }
-                            gettingPoint = false;
                         }
-                        else
+                        gettingPoint = false;
+                    }
+                    else
+                    {
+                        if (!gettingPoint)
                         {
-                            if (!gettingPoint)
-                            {
-                                currentDataPoint[dataPointNum % 3] = "";
-                            }
-                            gettingPoint = true;
-                            currentDataPoint[dataPointNum % 3] += fullText[i];
+                            currentDataPoint[dataPointNum % 3] = "";
                         }
+                        gettingPoint = true;
+                        currentDataPoint[dataPointNum % 3] += fullText[i];
                     }
                 }
-            return vector;
+                SR.Close();
+            }
+            float[] returnVector = new float[vector.Length / 2];
+            for (int i = 0; i < returnVector.Length; i++ )
+            {
+                returnVector[i] = vector[i, 0];
+            }
+                
+            return returnVector;
         }
     }
 }
